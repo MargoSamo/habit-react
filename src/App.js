432f606d39fc5ed6d29934ed623ab9d3b0b63f0b
MargoSamo <margo_ii@mail.ru>
2022-10-30 17:43:06 +0400

    create new react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const histories = [{
+    historyName: "run",
+    records: ["ACHIEVED", "FAILED", "ACHIEVED"]
+  },
+  {
+    historyName: "push ups",
+    records: ["ACHIEVED", "FAILED"]
+  },
+  {
+    historyName: "yoga", 
+    records: ["ACHIEVED", "FAILED"]
+  }]
+
+  var increment = 0
+  const listItems = histories.map((history) => {
+    const records = history.records.map((record) => {
+      increment++;
+      var isChecked = false;
+      if (record === 'ACHIEVED') {
+        isChecked = true;
+      } else {
+        isChecked = false;
+      }
+      
+      console.log(isChecked);
+      console.log(record);
+
+      return <input id={increment} type="checkbox" checked={isChecked} onChange={() => isChecked(!true)}/>;
+    });
+
+    return <div>{history.historyName} {records}</div>
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App"> {listItems} </div>
   );
 }
 
